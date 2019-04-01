2a3a0035f633924f8e43de066c6aead0df2797fd
JP Cimalando <jp-dev@inbox.ru>
2019-04-01 11:33:33 +0200

    add license

--- sources/vgm.cc
+++ sources/vgm.cc
@@ -1,3 +1,8 @@
+//          Copyright Jean Pierre Cimalando 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include "vgm.h"
 #include "mapped_file.h"
 extern "C" {
